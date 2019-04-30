c74a9774f5c0df826871fb3d27f7a45ab612faf6
mohammed chouta <mohammedchouta10@gmail.com>
2019-04-30 13:03:25 +0200

    Add files via upload

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,40 +1,27 @@
-import os
 from flask import Flask
-from . import (db, auth, blog)
+from .auth import Auth
+from .dashboard import Dashboard
+
+app = Flask(__name__)
+
+auth = Auth()
+dashB = Dashboard()
 
 
+@app.route('/')
+def showHome():
+	home.showHome()
 
-def create_app(test_config=None):
-    #create and configure the app
-    app = Flask(__name__, instance_relative_config=True) #creates the Flask instance
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
-    )
+@app.route('/register')
+def showRegister():
+	auth.showRegister()
 
-    if test_config is None:
-        #load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        #load the test config if passed in
-        app.config.from_mapping(test_config)
+@app.route('/login')
+def showLogin():
+	auth.showLogin()
+	
+@app.route('/dashboard')
+def showDashboard():
+	dashB.showDashboard
 
-    #ense the instance folder exits
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World'
-    
-    db.init_app(app)
-    
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
 
-    app.add_url_rule('/', endpoint="index")
-
-    return app
